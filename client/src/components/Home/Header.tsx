ae380c002111f055e30365d51ba3a74e69802896
riyandarmawan <riyandaramwan042@gmail.com>
2025-04-22 15:46:58 +0700

    refactor: home header ui

--- client/src/components/Home/Header.tsx
+++ client/src/components/Home/Header.tsx
@@ -62,7 +62,7 @@ export default function Header() {
           {/* theme toggle */}
           <DropdownMenuItem
             onClick={toggleTheme}
-            className="flex items-center gap-2"
+            className="flex items-center gap-2 cursor-pointer"
           >
             {theme === "light" ? (
               <>
